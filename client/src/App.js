a3e02d9a0cb700aae5b19614d3a8e8819f148336
levilu800b <levilu800b@gmail.com>
2023-05-15 14:43:39 +0100

    react router dom routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+/** root routes */
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <div>Root Route</div>
+	},
+	{
+		path: '/register',
+		element: <div>Register Route</div>
+	}
+])
 
 export default function App() {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
+		<main>
+			<RouterProvider router={router}></RouterProvider>
+		</main>
 	);
 }
