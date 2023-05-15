3d7e5b9e52f5dd0fae58baba267f59730d681fb5
levilu800b <levilu800b@gmail.com>
2023-05-15 15:12:12 +0100

    connection routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+/** import all components */
+import Username from './components/UserName/Username';
+import Password from './components/Password/Password';
+import Register from './components/Register/Register';
+import Profile from './components/Profile/Profile';
+import Reset from './components/Reset/Reset';
+import Recovery from './components/Recovery/Recovery';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+
 /** root routes */
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>Root Route</div>
+		element: <Username></Username>,
 	},
 	{
 		path: '/register',
-		element: <div>Register Route</div>
-	}
-])
+		element: <Register></Register>,
+	},
+	{
+		path: '/password',
+		element: <Password></Password>,
+	},
+	{
+		path: '/profile',
+		element: <Profile></Profile>,
+	},
+	{
+		path: '/recovery',
+		element: <Recovery></Recovery>,
+	},
+	{
+		path: '/reset',
+		element: <Reset></Reset>,
+	},
+	{
+		path: '*',
+		element: <PageNotFound></PageNotFound>,
+	},
+]);
 
 export default function App() {
 	return (
