e5c5ce3c362dd532c5e27fab54a40c727f854767
levilu800b <levilu800b@gmail.com>
2023-05-16 18:55:15 +0100

    setup backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+/** middleware */
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+const port = 8080;
+
+/** HTTP GET REQUEST */
+app.get('/', (req, res) => {
+    res.status(201).json("Home GET Request");
+});
+
+/** Start server */
+app.listen(port, () => {
+    console.log(`Server connected to http://localhost:${port}`);
+})
