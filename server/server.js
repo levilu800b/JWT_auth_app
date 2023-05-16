4a732b256fd92e43685959b0e4a01bcf411cdd70
levilu800b <levilu800b@gmail.com>
2023-05-16 19:19:01 +0100

    mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,23 +1,37 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import connect from './database/connection.js';
+//import router from './router/route.js';
 
 const app = express();
 
-/** middleware */
-
+/** middlewares */
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
 const port = 8080;
 
-/** HTTP GET REQUEST */
+/** HTTP GET Request */
 app.get('/', (req, res) => {
-    res.status(201).json("Home GET Request");
+	res.status(201).json('Home GET Request');
 });
 
-/** Start server */
-app.listen(port, () => {
-    console.log(`Server connected to http://localhost:${port}`);
-})
+/** api routes */
+//app.use('/api', router);
+
+/** start server only when we have valid connection */
+connect()
+	.then(() => {
+		try {
+			app.listen(port, () => {
+				console.log(`Server connected to http://localhost:${port}`);
+			});
+		} catch (error) {
+			console.log('Cannot connect to the server');
+		}
+	})
+	.catch((error) => {
+		console.log('Invalid database connection...!');
+	});
