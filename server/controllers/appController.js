2c0bfe01645144c1f55752a24fa3768efabdef60
levilu800b <levilu800b@gmail.com>
2023-05-17 21:02:21 +0100

    login

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,24 @@
 import UserModel from '../model/User.model.js';
 import bcrypt from 'bcrypt';
+import jwt  from 'jsonwebtoken';
+import ENV from '../config.js';
+
+/** middleware for verify user */
+export async function verifyUser(req, res, next){
+    try {
+        
+        const { username } = req.method == "GET" ? req.query : req.body;
+
+        // check the user existence
+        let exist = await UserModel.findOne({ username });
+        if(!exist) return res.status(404).send({ error : "Can't find User!"});
+        next();
+
+    } catch (error) {
+        return res.status(404).send({ error: "Authentication Error"});
+    }
+}
+
 
 /** POST: http://localhost:8080/api/register 
  * @param : {
@@ -93,7 +112,43 @@ export async function register(req, res) {
 */
 
 export async function login(req, res) {
-	res.json('register route');
+	const { username, password } = req.body;
+
+	try {
+		UserModel.findOne({ username })
+			.then((user) => {
+				bcrypt
+					.compare(password, user.password)
+					.then((passwordCheck) => {
+						if (!passwordCheck)
+							return res.status(400).send({ error: "Don't have Password" });
+
+						// create jwt token
+						const token = jwt.sign(
+							{
+								userId: user._id,
+								username: user.username,
+							},
+							ENV.JWT_SECRET,
+							{ expiresIn: '24h' },
+						);
+
+						return res.status(200).send({
+							msg: 'Login Successful...!',
+							username: user.username,
+							token,
+						});
+					})
+					.catch((error) => {
+						return res.status(400).send({ error: 'Password does not Match' });
+					});
+			})
+			.catch((error) => {
+				return res.status(404).send({ error: 'Username not Found' });
+			});
+	} catch (error) {
+		return res.status(500).send({ error });
+	}
 }
 
 /** GET: http://localhost:8080/api/user/example123 */
