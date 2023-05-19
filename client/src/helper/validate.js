c4a8e1ebafdf3a2f10fa4cff511ee476176a8065
levilu800b <levilu800b@gmail.com>
2023-05-19 22:02:34 +0100

    Zustand store

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,10 +1,20 @@
 import toast from 'react-hot-toast';
+import { authenticate } from './helper';
 
 /** validate login page username */
-export function usernameValidate(values) {
-	const errors = usernameVerify({}, values);
+export async function usernameValidate(values){
+    const errors = usernameVerify({}, values);
 
-	return errors;
+    if(values.username){
+        // check user exist or not
+        const { status } = await authenticate(values.username);
+        
+        if(status !== 200){
+            errors.exist = toast.error('User does not exist...!')
+        }
+    }
+
+    return errors;
 }
 
 /** validate password */
