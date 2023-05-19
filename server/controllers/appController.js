347d46ece9aad707969a33fd239886052cd91d8d
levilu800b <levilu800b@gmail.com>
2023-05-19 21:34:00 +0100

    axios frontend

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -177,7 +177,7 @@ export async function getUser(req, res) {
 	}
 }
 
-/** PUT: http://localhost:8080/api/updateuser 
+/** PUT: http://localhost:8080/api/updateUser 
  * @param: {
   "header" : "<token>"
 }
