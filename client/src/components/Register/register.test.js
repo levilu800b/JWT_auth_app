225859dde4615ed4e19c672c5174a290abb936cf
levilu800b <levilu800b@gmail.com>
2023-05-24 17:05:16 +0100

    installed and ran first cypress test

--- client/src/components/Register/register.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Register from './Register';
-
-test('renders button with correct label', () => {
-	render(<Register label="Click me" />);
-	const buttonElement = screen.getByText(/click me/i);
-	expect(buttonElement).toBeInTheDocument();
-});
