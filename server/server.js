a688245042469f09952220543e60bbbd64c03c18
levilu800b <levilu800b@gmail.com>
2024-05-15 02:51:16 +0100

    try again

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 /** HTTP GET Request */
 app.get('/', (req, res) => {
